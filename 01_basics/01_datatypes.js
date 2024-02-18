e0408925843aa3af49ff5eda30df2eae56cbe27d
Vaibhav Nathe <119159679+vaibhavnathe@users.noreply.github.com>
2024-02-18 23:37:48 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  // treat all js code as newer version
+
+// alert("Hello")  // we are using nodejs , not browser
+
+console.log(3+3)           // code readability should be high
+console.log("Vaibhav")
+
+
+// Datatypes
+
+let name = "Vaibhav"    // string
+let age = 20            // number
+let isLoggedIn = false  // boolean
+
+// number => 2^53
+// bigint
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbols => unique
+
+// object
+
+console.log(typeof("Vaibhav"));
+console.log(typeof(age));
+console.log(typeof(null));      // object
+console.log(typeof(undefined));      // undefined
