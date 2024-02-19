dbec6815b2e708c19cc160ca3f9070e7c30a4d6d
Vaibhav Nathe <119159679+vaibhavnathe@users.noreply.github.com>
2024-02-19 07:53:39 +0000

    Conversion of datatypes

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33anc"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let valueInNumber = Number(score)     // score is converted into
+// console.log(typeof valueInNumber);      // number
+// console.log((valueInNumber));       // NaN because score="33abc"
+
+// "33" => 33 (number)
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
