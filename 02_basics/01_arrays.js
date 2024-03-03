8d15d7a7ce1e1860aebbe3d1f69252cf36a4c6ba
Vaibhav Nathe <119159679+vaibhavnathe@users.noreply.github.com>
2024-03-03 08:52:04 +0000

    Arrays in JS part1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// array
+
+const myArr = [0,1,2,3,4,5];
+const myHeroes = ["IronMan", "Shaktiman"];
+
+const myArr2 = new Array(1,2,3);
+
+// console.log(myArr[0]);
+
+//Array methods
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+
+// myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join();    // bind & converts into the string
+
+// console.log(myArr);
+// console.log(typeof newArr); // string
+
+
+// slice, splice
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+console.log("C ", myArr);
